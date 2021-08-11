7544724b77ff343c3636fd8ef984a4e1003c9328
Ben Grant <benjah88@gmail.com>
2021-08-11 08:37:15 -0700

    update

---
+++ src/components/Program.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Program extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        title: "Welcome",
+        start: false,
+        resultsTitle: "Program Results"
+      };
+      this.handleStart = this.handleStart.bind(this);
+    }
+
+    handleStart(){
+        this.setState({start: true, title: "Loading..."})
+    }
+  
+    render() {
+      return (
+        <div>
+            <h1>{this.state.title}</h1>
+            <button onClick={this.handleStart}>Start Program</button>
+            <h2>{this.state.resultsTitle}</h2>
+        </div>
+      );
+    }
+  }
+
+  export default Program;
